0c9698c8e3c055606419372ea2ede3d215e0dcf7
Bishal Thapa <bthapa@ucdavis.edu>
2018-05-15 20:31:56 -0700

    Onto Part 3

--- server/SnDserver2.js
+++ server/SnDserver2.js
@@ -1,19 +1,21 @@
 var serverPort = 53381;
 var localPort = 8000;
 
-
 var http  = require('http');
 var static = require('node-static');
 var fs = require('fs');
+var url = require('url');
+var sqlite3 = require('sqlite3');
 
-var queryString = "query?num=";
+var queryString = "query";
 var imgURLBase = "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/";
+var sql = "SELECT * FROM photoTags WHERE rowid IN ";
 
-var imgList = [];
-loadImageList();
+let dbFileName = "PhotoQ.db";
+var db = new sqlite3.Database(dbFileName);
+
 
 function handler (request, response) {
-
     if(request.url.indexOf(queryString) >= 0){
         queryImgHandler(request,response);
         //console.log("DYNAM");
@@ -24,18 +26,24 @@ function handler (request, response) {
     }
 }
 
-function loadImageList () {
-    var data = fs.readFileSync('photoList.json');
-    if (! data) {
-	    console.log("cannot read photoList.json");
-    } else {
-	    listObj = JSON.parse(data);
-	    imgList = listObj.photoURLs;
-    }
-}
-
 function queryImgHandler(request, response) {
-    var url = request.url.replace("/","");
+    var req = url.parse(request.url, true);
+    var indicies = '(' + req.query.numList.split(" ").join(",") + ')';
+
+    var responseObject = [];
+    var sqlCmd = sql + indicies;
+
+    db.each(sqlCmd, function(err, data){
+        if(err) console.err("YA FUCKED UP");
+        responseObject.push({
+            src: imgURLBase+data.fileName,
+            width: data.width,
+            height: data.height,
+        });
+        console.log(responseObject);
+    });
+
+    /*
     var queryNum = url.substring(url.indexOf("=") + 1, url.length);
     if(queryNum && queryNum >= 0 && queryNum <= 989){
         response.writeHead(200, {"Content-Type": "text/html"});
@@ -47,6 +55,7 @@ function queryImgHandler(request, response) {
         response.write("<p>Bad Request</p>");
         response.end();
     }
+    */
 }
 
 function staticHandler(request, response){
@@ -65,3 +74,25 @@ var server = http.createServer(handler);
 var fileServer = new static.Server('./public');
 //server.listen(serverPort);
 server.listen(localPort, '127.0.0.1');
+
+
+//var imgList = [];
+//loadImageList();
+/*
+function loadImageList () {
+    var data = fs.readFileSync('photoList.json');
+    if (! data) {
+	    console.log("cannot read photoList.json");
+    } else {
+	    listObj = JSON.parse(data);
+	    imgList = listObj.photoURLs;
+    }
+}
+
+function dumpDB() {
+  db.all( 'SELECT * FROM photoTags WHERE rowid IN (1, 2)', dataCallback);           // get the specific rows
+      function dataCallback( err, data ) {
+		console.log(data)
+      }
+}
+*/
--- server/loadNames.js
+++ server/loadNames.js
@@ -1,64 +1,72 @@
 var url = require('url');
 var http = require('http');
-var imageJson = require('./6whs.json');
 var sqlite3 = require("sqlite3").verbose();  // use sqlite
 var fs = require("fs");                      // use file-system library
-var sizeOf = require('image-size');             //
+//var imageJson = require('./6whs.json');
+var imageJson = JSON.parse(fs.readFileSync("photoList.json")).photoURLs;
+var sizeOf = require('image-size');           //
+
+
+http.globalAgent.maxSockets = 1;
 
 let dbFileName = "PhotoQ.db";
+var imgServerURL = 'http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/'
+var sql = 'INSERT INTO photoTags(idNum, fileName, width, height, locTag, tags) VALUES(?,?,?,?,?,?)';
 // makes the object that represents the database in our code
 var db = new sqlite3.Database(dbFileName);
 
 // [idNum INTEGER UNIQUE NOT NULL PRIMARY KEY] [fileName TEXT] [width INTEGER] [height INTEGER] [locTag TEXT] [tags TEXT]
 
-var questionComma = "?,"
-
-let numPhotos = 6;
 let globalCounter = 0;
 
-for(var index = 0; index < numPhotos; index++){
-    getSize(index, imageJson[index].url, sizeCb);
+
+for(var index = 0; index < imageJson.length; index++){
+    //console.log(imageJson[index]);
+    getSize(index, imageJson[index], sizeCB);
 }
-/*
-imageJson.forEach( function(image){   // run through the imageJson files, adds them into table
-    getSize(globalCounter, image.url, sizeCb);
-});
-*/
+
 function getSize(ind, name, cbFun) {
-    console.log("getSize (Id):"+ind);
-    // var imgURL = imgServerURL+name;  // This is for the later shit.
-    // var options = url.parse(imgURL);
-    var options = url.parse(name);    // This is for the time-being development
+    //console.log("getSize (Id):"+ind);
+     var imgURL = imgServerURL + name;  // This is for the later shit.
+     var options = url.parse(imgURL);
+
+    //var options = url.parse(name);    // This is for the time-being development
 
     http.get(options, function (response) {
-	var chunks = [];
-	response.on('data', function (chunk) {
-	    chunks.push(chunk);
-	}).on('end', function() {
-	    var buffer = Buffer.concat(chunks);
-	    dimensions = sizeOf(buffer);
-	    cbFun(ind, name, dimensions.width, dimensions.height, dbCb);
-    });
+    	var chunks = [];
+    	response.on('data', function (chunk) {
+    	    chunks.push(chunk);
+    	}).on('end', function() {
+            console.log("getSize(ID)"+ ind);
+    	    var buffer = Buffer.concat(chunks);
+    	    dimensions = sizeOf(buffer);
+            if(dimensions) cbFun(ind, name, dimensions.width, dimensions.height);
+        });
+    }).on('error', function(err) {
+       // handle errors with the request itself
+       console.log(imgURL);
+       console.log(options);
+       console.error('Error with the request:', err.message);
+       //console.error('Error with the request:', err);
    });
 }
 
-function sizeCb(ind, name, width, height, dbCb){
-    var values = [ind,name,width,height,'',''];0
+function sizeCB(ind, name, width, height){
+    var values = [ind,name,width,height,'',''];
     console.log("sizeCB (Id):"+ind);
-    var sql = 'INSERT INTO photoTags(idNum, fileName, width, height, locTag, tags) VALUES(?,?,?,?,?,?)';
     db.run(sql, values, function(err) {            // insert shit
        if (err) {
          return console.log(err.message);
        }
        console.log(`A row has been inserted with rowid ${this.lastID}`);
-       dbCb();
+       dbCB();
      });
 }
 
-function dbCb(){
+function dbCB(){
     globalCounter++;
     console.log("dbCB (GLOBAL):"+globalCounter);
-    if(globalCounter == numPhotos){
+    if(globalCounter == imageJson.length){
         dumpDB();
         db.close();
     }
